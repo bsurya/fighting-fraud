1085c046b85133e88442faff963d25733bcbd989
bhargav <bhargav@10.0.0.59>
2016-12-20 20:11:33 -0800

    Initial commit to test dockerized jenkins continues integration.

---
+++ Risk-Commons/src/main/java/com/intellytix/util/JenkinsBuildTest.java
@@ -0,0 +1,8 @@
+package com.intellytix.util;
+
+public class JenkinsBuildTest {
+
+	public static void main(String args[]){
+		System.out.println("This is a test class for jenkins build");
+	}
+}
---
+++ Risk-Commons/src/main/resources/com/intellytix/util/JunitTest.java
@@ -0,0 +1,15 @@
+package com.intellytix.util;
+
+
+public class JunitTest {
+
+	@Before
+	public void initialize(){
+		
+	}
+	
+	@Test
+	public void testRun(){
+		
+	}
+}
